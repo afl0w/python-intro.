23dd984b23e93ae6311db529ba3b951b3b6145a9
Alexis Flores <flrs.alexis@gmail.com>
2019-12-30 20:54:52 -0800

    additional comments to the code
    added comments to the code to describe its functions.

--- diceRollgame.py
+++ diceRollgame.py
@@ -1,11 +1,12 @@
 import random 
-
+#Numbers on dice start from 0 - 6 
+#Action to perform random number from 0 -6 
 def Rolldice(rolls):
     for i in range(0,rolls):
-        number = random.randint(1,6)
+        number = random.randint(i,6)
         print(number)
     Menu()
-
+#Dice game Main Menu for user
 def Menu():
     print('1. Roll a Dice')
     print('2. Roll both Dice')
@@ -13,7 +14,7 @@ def Menu():
     print('3. Exit Program')
     print()
     Menu_choice = int(input('Enter Here: '))
-
+#Calculation/action based on the number inputed by user
     if (Menu_choice == 1):
         Rolldice(1)
     if (Menu_choice == 2):
