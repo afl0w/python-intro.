46705416ebdefac581b4d75cb55a27767c463f5a
Alexis Flores <flrs.alexis@gmail.com>
2020-03-25 22:26:09 -0700

    Add files via upload
    really basic 'while' loop

---
+++ while.py
@@ -0,0 +1,5 @@
+
+x = 0       # zero is the starting point
+while x < 10:   # while x remains less than 10
+    print(x)    #print the numbers of X
+    x += 1     # adds 1 to the count, 0,1,2,3, etc.
