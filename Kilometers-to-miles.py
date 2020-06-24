b94b620a9bd0b3746120da1f98adfd8e551e70c7
Alexis Flores <flrs.alexis@gmail.com>
2020-06-23 20:30:58 -0700

    Add files via upload
    A  small program that converts Kilometers to miles.

---
+++ Kilometers-to-miles.py
@@ -0,0 +1,8 @@
+
+#program that converts Kilometers to miles.
+km = int(input("Enter integer in Kilometers: "))
+
+#converting km to mi
+miles = .62137119 * km 
+
+print(km, "kilometers converts to", miles, "miles.")   
